eec74bff07a886d950a540e39c17eb755b59b040
Ashwin Bhat <ABhat@athene.com>
2020-11-23 08:40:42 -0600

    step 1-10 frame

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 """
 main file to generate ails based on admin system
 """
-from importlib import import_module
+#from importlib import import_module
 import sys
 import argparse
 import datetime
-from _datetime import date
+import os
 
 sys.path.insert(0, './ail_mesh')
 sys.path.insert(0, './core_utils')
 sys.path.insert(0, './afdm_attribution_ail')
 
-from transform import Transformer
+from transform import Transform
 
 def parse_timestamp(timestamp):
     """
@@ -28,20 +28,23 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-v','--valuation-date',
-                        type=parse_timestamp,
                         help='date for which the AILs should be generated')
     parser.add_argument('-s', '--admin-system',
                         help='name of the source system')
     parser.add_argument('-c', '--current-path',
                         help='path to the current input AILs')
-    parser.add_argument('-o', '--old-path',
+    parser.add_argument('-p', '--previous-path',
                         help='path to the old input AILs')
-
+    parser.add_argument('-o', '--output',
+                        help='path to the output AILs',
+                        default='output/test.ail2')
     args = parser.parse_args()
-
-    #source=import_module(args.admin_system)
     
-    Transformer(args)
+    #source=import_module(args.admin_system)
+    if not os.path.exists('output/'+args.valuation_date):
+        os.mkdir('output/'+args.valuation_date)
+    
+    Transform(args)
     
     
     
@@ -49,4 +52,4 @@ def main():
 if __name__ == '__main__':
     s=datetime.datetime.now()
     main()
-    print(datetime.datetime.now()-s)
+    print('total time-taken = ',datetime.datetime.now()-s)
--- transform.py
+++ transform.py
@@ -3,77 +3,219 @@ calculate/transform the required fields depending on admin system
 '''
 import tqdm
 #from core_utils.progress import sane_tqdm
-from core_utils.tabular import FastDictReader
-from core_utils.tabular import CSVDataIO
+#from core_utils.tabular import FastDictReader
+#from core_utils.tabular import CSVDataIO
 #from collections import OrderedDict
+from core_utils.core_utils.tabular import FastDictReader
+from core_utils.core_utils.tabular import CSVDataIO
 
-class Transformer:
+class Transform:
     '''
-    use transformation based on adminsystem
+    generate step 1- 10 atttribution_ails
     '''
     def __init__(self, args):
         '''
         initialize the class object
         '''
         self.args = args
+        self.cur = args.current_path
+        self.prev = args.previous_path
+        self.fieldnames = []
+        file_name = args.current_path.split('\\')
+        self.output_path = 'output/'+args.valuation_date+'/{}_'+file_name[-1]
         self.generate()
 
     def generate(self):
         '''
         read transform and create output files
         '''
-        self.cur_ail, self.prev_ail = self.reader()
-        self.output = self.writer()
-        self.transformer()
+        self.step_1()
+        self.step_2()
+        self.step_3()
+        self.step_4()
+        self.step_5()
+        self.step_6()
+        self.step_7()
+        self.step_8()
+        self.step_9()
+        self.step_10()
 
-    def transformer(self):
+    def transformer(self, input_1, input_2, output, step, *field):
         '''
         transform logic
         '''
-        progress = tqdm.tqdm(mininterval=1, unit=' rows', desc='rows checked')
-        cur_row = next(self.cur_ail)
-        prev_row = next(self.prev_ail)
+        progress = tqdm.tqdm(mininterval=1, unit=' rows', desc='rows checked '+step)
+        cur_row = next(input_1)
+        prev_row = next(input_2)
         while cur_row:
             try:
                 if prev_row['Company']+prev_row['PolNo'] < cur_row['Company']+cur_row['PolNo']:
-                    prev_row = next(self.prev_ail)
-                    continue
+                    prev_row = next(input_2)
                 elif prev_row['Company']+prev_row['PolNo'] > cur_row['Company']+cur_row['PolNo']:
-                    self.output.send(cur_row)
+                    output.send(cur_row)
+                    cur_row = next(input_1)
                 else:
-                    self.output.send(cur_row)
-                cur_row = next(self.cur_ail)
-                prev_row = next(self.prev_ail)
+                    #operations
+                    output.send(cur_row)
+                    cur_row = next(input_1)
+                    prev_row = next(input_2)
             except StopIteration:
                 break
             progress.update()
         try:
+            cur_row = next(input_1)
             while cur_row:
-                self.output.send(cur_row)
-                cur_row = next(self.cur_ail)
+                output.send(cur_row)
+                cur_row = next(input_1)
         except StopIteration:
-            print('done')
+            progress.update()
+        progress.close()
+            #print('\ndone')
 
-    def reader(self):
+    def reader(self, file_1, file_2):
         '''
         reader
         '''
-        print('current read')
-        new_file = open(self.args.current_path)
-        obj1 = FastDictReader(new_file, delimiter='\t')
+        #print('Inputs read')
+        fp_1 = open(file_1)
+        fp_2 = open(file_2)
+        obj1 = FastDictReader(fp_1, delimiter='\t')
         self.fieldnames = obj1.fieldnames
-        print('previous read')
-        old_file = open(self.args.old_path)
-        obj2 = FastDictReader(old_file, delimiter='\t')
+        obj2 = FastDictReader(fp_2, delimiter='\t')
         return obj1, obj2
 
-    def writer(self):
+    def writer(self, step):
         '''
         writer
         '''
-        print('write')
+        #print('output write')
         write = CSVDataIO(delimiter='\t')
         cols = self.fieldnames
-        file = write.iterative_writer('test.ail', cols)
+        file = write.iterative_writer(self.output_path.format(step), cols)
 #        write.write_file('test.ail', cols, self.res.values())
         return file
+
+#     def close(self, fp_1, fp_2):
+#         '''
+#         close the files
+#         '''
+#         fp_1.close()
+#         fp_2.close()
+
+    def step_1(self):
+        '''
+        step 1
+        '''
+        print('step - 1')
+        cur, prev = self.reader(self.cur, self.prev)
+        output = self.writer('step_1')
+        self.transformer(cur, prev, output, 'step-1')
+        #self.close(cur,  prev)
+        self.prev = self.cur
+        self.cur = 'step_1.ail2'
+
+    def step_2(self):
+        '''
+        step 2
+        '''
+        print('step - 2')
+        cur, prev = self.reader(self.cur, self.prev)
+        output = self.writer('step_2')
+        self.transformer(cur, prev, output, 'step-2')
+        #self.close(cur, prev)
+        self.prev = self.cur
+        self.cur = 'step_2.ail2'
+
+    def step_3(self):
+        '''
+        step 3
+        '''
+        print('step - 3')
+        cur, prev = self.reader(self.cur, self.prev)
+        output = self.writer('step_3')
+        self.transformer(cur, prev, output, 'step-3')
+        #self.close(cur, prev)
+        self.prev = self.cur
+        self.cur = 'step_3.ail2'
+
+    def step_4(self):
+        '''
+        step 4
+        '''
+        print('step - 4')
+        cur, prev = self.reader(self.cur, self.prev)
+        output = self.writer('step_4')
+        self.transformer(cur, prev, output, 'step-4')
+        #self.close(cur, prev)
+        self.prev = self.cur
+        self.cur = 'step_4.ail2'
+
+    def step_5(self):
+        '''
+        step 5
+        '''
+        print('step - 5')
+        cur, prev = self.reader(self.cur, self.prev)
+        output = self.writer('step_5')
+        self.transformer(cur, prev, output, 'step-5')
+        #self.close(cur, prev)
+        self.prev = self.cur
+        self.cur = 'step_5.ail2'
+
+    def step_6(self):
+        '''
+        step 6
+        '''
+        print('step - 6')
+        cur, prev = self.reader(self.cur, self.prev)
+        output = self.writer('step_6')
+        self.transformer(cur, prev, output, 'step-6')
+        #self.close(cur, prev)
+        self.prev = self.cur
+        self.cur = 'step_6.ail2'
+
+    def step_7(self):
+        '''
+        step 7
+        '''
+        print('step - 7')
+        cur, prev = self.reader(self.cur, self.prev)
+        output = self.writer('step_7')
+        self.transformer(cur, prev, output, 'step-7')
+        #self.close(cur, prev)
+        self.prev = self.cur
+        self.cur = 'step_7.ail2'
+
+    def step_8(self):
+        '''
+        step 8
+        '''
+        print('step - 8')
+        cur, prev = self.reader(self.cur, self.prev)
+        output = self.writer('step_8')
+        self.transformer(cur, prev, output, 'step-8')
+        #self.close(cur, prev)
+        self.prev = self.cur
+        self.cur = 'step_8.ail2'
+
+    def step_9(self):
+        '''
+        step 9
+        '''
+        print('step - 9')
+        cur, prev = self.reader(self.cur, self.prev)
+        output = self.writer('step_9')
+        self.transformer(cur, prev, output, 'step-9')
+        #self.close(cur, prev)
+        self.prev = self.cur
+        self.cur = 'step_9.ail2'
+
+    def step_10(self):
+        '''
+        step 10
+        '''
+        print('step - 10')
+        cur, prev = self.reader(self.cur, self.prev)
+        output = self.writer('step_10')
+        self.transformer(cur, prev, output, 'step-10')
+        #self.close(cur, prev)
