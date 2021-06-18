512bd4ee9ef5ebc731d8848d8dbd7e7f2a8b8da3
Prashant Sachdeva <E44595@atheneusa.com>
2021-06-18 09:06:28 -0500

    ACTP-611 ACTP-612 adding step5 and step6 changes

--- afdm_attribution_ail/step5.py
+++ afdm_attribution_ail/step5.py
@@ -2,15 +2,51 @@
 transformation step-5
 '''
 #from decimal import Decimal
+from prework import Prework
+from datetime import date
 
 class Step5:
     '''
     changes to be made in step 5
     '''
-    def __init__(self):
+    def __init__(self, valuation_date):
         '''
         define fields to be modified in step 5
         '''
-        self.functions = []
-
+        self.valuation_date = date(int(valuation_date[:4]), int(valuation_date[4:6]), int(valuation_date[6:]))
+        self.fields = ['GMWBCharge','GMWBMaxBenTable','GMWBInitAccumPeriod','GMWBMaxAccumPeriod','GMWBMaxCharge',\
+                        'AdditionalRestartMonths','RestartCharge','Seed']
+        self.functions = [self.step_5,
+                          self.IRRestartNew]
+                          
+    
+    def IRRestartNew(self, merger_row, current_row, fieldnames):
+        '''
+        logic for the field
+        '''
+        if 'IRRestartNew' in fieldnames:
+            if merger_row['IRRestartNew_PQ'] in ("F","X") and merger_row['IRRestartNew_CQ'] in ("Y","N"):
+                if merger_row['IRRestartNew_PQ'] == 'F':
+                    current_row['IRRestartNew'] = 'Y'
+                else:
+                    current_row['IRRestartNew'] = 'N'
+        return current_row
+    
+    def Step5_functions(self, merger_row, current_row, field):
+        '''
+        logic for the field
+        '''
+        if merger_row['join_indicator'] == 'AB' and merger_row['IRRestartNew_PQ'] in ("F","X") and \
+        merger_row['IRRestartNew_CQ'] in ("Y","N"):
+            current_row[f'{field}'] = merger_row[f'{field}'+'_PQ']
+        
+        return current_row
+    
+    def step_5(self,merger_row,current_row,fieldnames):
+        
+        for field in self.fields:
+            if field in fieldnames:
+                current_row = self.Step5_functions(merger_row,current_row,field)
+            
+        return current_row
     
--- afdm_attribution_ail/step6.py
+++ afdm_attribution_ail/step6.py
@@ -2,15 +2,44 @@
 transformation step-6
 '''
 #from decimal import Decimal
+from prework import Prework
+from datetime import date
+from collections import OrderedDict
+from Columns import AIL_Columns as order
 
 class Step6:
     '''
     changes to be made in step 6
     '''
-    def __init__(self):
+    def __init__(self, valuation_date):
         '''
         define fields to be modified in step 6
         '''
-        self.functions = []
-
+        self.valuation_date = date(int(valuation_date[:4]), int(valuation_date[4:6]), int(valuation_date[6:]))
+        
+        self.fields = ['GMWBCharge','GMWBIncType','GMWBParRate','GMWBParRate2','GMWBParRate3','GMWBParRate4',\
+                       'GMWBPayment','GMWBRollup','GMWBRollup2','GMWBRollup3','GMWBRollup4','GMWBMaxAccumPeriod',\
+                       'GMWBMaxCharge','F133GMWBPayment','IRRestartNew','InitialRestartMonths','AdditionalRestartMonths',\
+                       'RestartCharge','Seed']
+        
+        self.functions = [self.step_6]
+                          
+    
+    
+    def Step6_functions(self, merger_row, current_row, field):
+        '''
+        logic for the field
+        '''
+        if merger_row['join_indicator'] == 'AB' and merger_row['IRRestartNew_PQ'] not in ( "U", "_") and\
+         merger_row['IRRestartNew_CQ'] == 'U':
+            current_row[f'{field}'] = merger_row[f'{field}'+'_PQ']
+        return current_row
+    
+    def step_6(self,merger_row,current_row,fieldnames):
+        
+        for field in self.fields:
+            if field in fieldnames:
+                current_row = self.Step6_functions(merger_row,current_row,field)
+            
+        return current_row
     
