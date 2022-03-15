af146077bea5fba17a13d93d01ffa2a74faca8cd
E45061 <e45061@atheneusa.com>
2022-03-15 09:36:07 -0500

    Updating maturity logic for Rocky

--- bin/value.py
+++ bin/value.py
@@ -224,7 +224,7 @@ class INTOutput():
         return datetime.datetime(_year,_month, _day).date()
 
     def get_maturity_date(self, valdate, issue_date_pq, idx, term):
-        if self.args.block in ('voya_fia', 'voya_fa', 'jackson.fia', 'jackson.tda'):
+        if self.args.block in ('voya_fia', 'voya_fa', 'jackson.fia', 'jackson.tda', 'Rocky.tda', 'Rocky.fda'):
             if float(self.irecord[f'Idx{idx}AVIF_PQ']) <= 0:
                 term = 1
             mat_yr = valdate.year - (valdate.year - issue_date_pq.year) % term
